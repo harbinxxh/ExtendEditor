d1b6825626ea4f03998227bd2bebb5ad707217e5
xxh <harbinxxh@gmail.com>
2023-04-09 11:02:51 +0800

    [Quick Asset Actions][Asset Action Utility] Built-in editor scripting class

--- Plugins/SuperManager/Source/SuperManager/SuperManager.Build.cs
+++ Plugins/SuperManager/Source/SuperManager/SuperManager.Build.cs
@@ -18,6 +18,7 @@ public class SuperManager : ModuleRules
 		PrivateIncludePaths.AddRange(
 			new string[] {
 				// ... add other private include paths required here ...
+				System.IO.Path.GetFullPath(Target.RelativeEnginePath) + "Source/Editor/Blutility/Private"
 			}
 			);
 			
@@ -25,7 +26,7 @@ public class SuperManager : ModuleRules
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
-				"Core",
+				"Core","Blutility"
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
