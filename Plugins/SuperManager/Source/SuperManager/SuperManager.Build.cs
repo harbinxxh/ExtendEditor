9513c462fff07446af83112051085b34861bc162
xxh <harbinxxh@gmail.com>
2023-04-10 11:14:44 +0800

    [Quick Asset Actions][Asset Batch Duplication] UEditorAssetLibrary and UEditorUtilityLibrary

--- Plugins/SuperManager/Source/SuperManager/SuperManager.Build.cs
+++ Plugins/SuperManager/Source/SuperManager/SuperManager.Build.cs
@@ -26,7 +26,7 @@ public class SuperManager : ModuleRules
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
-				"Core","Blutility"
+				"Core","Blutility","EditorScriptingUtilities"
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
