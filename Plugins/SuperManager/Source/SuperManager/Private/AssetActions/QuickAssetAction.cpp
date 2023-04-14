d160ee448b4cc8ea5b3defb8e255378612b11f8d
xxh <harbinxxh@gmail.com>
2023-04-14 10:26:14 +0800

    [Quick Asset Actions][Add Prefixes To Assets] How to use TMap

--- Plugins/SuperManager/Source/SuperManager/Private/AssetActions/QuickAssetAction.cpp
+++ Plugins/SuperManager/Source/SuperManager/Private/AssetActions/QuickAssetAction.cpp
@@ -39,3 +39,41 @@ void UQuickAssetAction::DuplicateAssets(int32 NumOfDuplicates)
 		ShowNotifyInfo(TEXT("Successfully duplicated " + FString::FromInt(Counter) + " files"));
 	}
 }
+
+void UQuickAssetAction::AddPrefixes()
+{
+	TArray<UObject*> SelectedObjects = UEditorUtilityLibrary::GetSelectedAssets();
+	uint32 Counter = 0;
+
+	for (UObject* SelectedObject:SelectedObjects)
+	{
+		if (!SelectedObject) continue;
+		
+		FString* PrefixFound = PrefixMap.Find(SelectedObject->GetClass());
+
+		if (!PrefixFound || PrefixFound->IsEmpty())
+		{
+			Print(TEXT("Failed to find prefix for class ") + SelectedObject->GetClass()->GetName(), FColor::Red);
+			continue;
+		}
+
+		FString OldName = SelectedObject->GetName();
+
+		if (OldName.StartsWith(*PrefixFound))
+		{
+			Print(OldName + TEXT(" already has prefix added"), FColor::Red);
+			continue;
+		}
+
+		const FString NewNameWithPrefix = *PrefixFound + OldName;
+
+		UEditorUtilityLibrary::RenameAsset(SelectedObject, NewNameWithPrefix);
+
+		++Counter;
+	}
+
+	if (Counter > 0)
+	{
+		ShowNotifyInfo(TEXT("Successfully rename " + FString::FromInt(Counter) + " assets"));
+	}
+}
