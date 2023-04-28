0f3bfc765cc0008884fc2cf5cce8fbd9b22289ae
xxh <harbinxxh@gmail.com>
2023-04-28 08:19:15 +0800

    [Extend Content Browser Menu][Add Custom Delegate] Add custom delegate to existing array

--- Plugins/SuperManager/Source/SuperManager/Private/SuperManager.cpp
+++ Plugins/SuperManager/Source/SuperManager/Private/SuperManager.cpp
@@ -1,12 +1,14 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "SuperManager.h"
+#include "ContentBrowserModule.h"
 
 #define LOCTEXT_NAMESPACE "FSuperManagerModule"
 
 void FSuperManagerModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	InitCBMenuExtention();
 }
 
 void FSuperManagerModule::ShutdownModule()
@@ -14,6 +16,23 @@ void FSuperManagerModule::ShutdownModule()
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
 }
+
+#pragma region ContentBrowserMenuExtention
+
+void FSuperManagerModule::InitCBMenuExtention()
+{
+	FContentBrowserModule& ContentBrowserModule =
+	FModuleManager::LoadModuleChecked<FContentBrowserModule>(TEXT("ContentBrowser"));
+
+	TArray<FContentBrowserMenuExtender_SelectedPaths>& ContentBrowserModuleMenuExtenders = 
+	ContentBrowserModule.GetAllPathViewContextMenuExtenders();
+
+	FContentBrowserMenuExtender_SelectedPaths CustomCBMenuDelegate;
+
+	ContentBrowserModuleMenuExtenders.Add(CustomCBMenuDelegate);
+}
+
+#pragma endregion
 
 #undef LOCTEXT_NAMESPACE
 	
